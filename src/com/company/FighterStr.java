fbbbc6ce940bfc590bb20f0f54e29943e50ab67a
Yuliya513 <83278354+Yuliya513@users.noreply.github.com>
2021-10-09 15:18:52 +0300

    Some strings are described

---
+++ src/com/company/FighterStr.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class FighterStr {
+
+    @Override
+    public String toString() {
+        return "FighterStr{}";
+    }
+
+    public static void main(String[] args) {
+	String FirstName;
+	String LastName;
+	String FullName;
+        class Fighter(object):
+        def __init__(self, name, health, damage_per_attack):
+        self.name = name
+        self.health = health
+        self.damage_per_attack = damage_per_attack
+
+        def __str__(self):
+        return "Fighter({}, {}, {})".format(self.name, self.health, self.damage_per_attack)
+        __repr__=__str__
+
+
+
+	int age;
+	int type;
+        System.out.println("age");
+        System.out.println("type");
+
+
+    }
+}
